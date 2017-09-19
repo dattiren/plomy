6d3e4f884b1e2443be26d40ab52121fd8d0623af
atsuo1203 <dondonkitemasu@gmail.com>
2017-09-20 02:23:51 +0900

    cellの高さを自動調整

--- plomy/HomeTableViewController.swift
+++ plomy/HomeTableViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
-
+    var articles: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.estimatedRowHeight = 200
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
+        articles = [
+            "私わたくしはその人を常に先生と呼んでいた。だからここでもただ先生と書くだけで本名は打ち明けない。",
+            "この書の世に出づるにいたりたるは、函館にある秦慶治氏、及び信濃にある神津猛氏のたまものなり。労作終るの日にあたりて、このものがたりを二人の恩人のまへにさゝぐ。",
+            "散文に二種あると考へてゐるが、一を小説、他を作文とかりに言つておく。",
+            "機敏な晩熟児といふべき此の男が、現に存するのだから僕は機敏な晩熟児が如何にして存るママかその様を語らうと思ふ。",
+        ]
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -34,14 +45,17 @@ class HomeTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 10
+        return articles.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
         //let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as! PostTableViewCell
 
+        cell.userName.text = indexPath.row.description + "番目のcell"
+        cell.containText.text = articles[indexPath.row]
+        cell.layoutIfNeeded()
         return cell
     }
     
