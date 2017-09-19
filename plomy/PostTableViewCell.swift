a5991bd5a17060c4fa173090d6e9bce06bf2b89d
dattiren <ren@cps.im.dendai.ac.jp>
2017-09-20 01:36:54 +0900

    show sample table view

--- plomy/PostTableViewCell.swift
+++ plomy/PostTableViewCell.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class PostTableViewCell: UITableViewCell {
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var containText: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
