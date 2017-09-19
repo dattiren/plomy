672857b9c8aa03229f6610f36019a52055a6312e
dattiren <ren@cps.im.dendai.ac.jp>
2017-09-20 01:07:41 +0900

    add Table View

---
+++ plomy/CustomClass.swift
@@ -0,0 +1,14 @@
+//
+//  CustomClass.swift
+//  plomy
+//
+//  Created by ren on 2017/09/20.
+//  Copyright © 2017年 ren. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomLabel: UILabel {
+    
+}
